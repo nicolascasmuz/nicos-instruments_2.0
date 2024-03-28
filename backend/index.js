4b455f40967c3ad42a89c0c51ba22f6a518a11e0
Nicolás Casmuz <nicolascasmuz@gmail.com>
2024-03-27 22:15:50 -0300

    commit-019

--- backend/index.js
+++ backend/index.js
@@ -29,9 +29,9 @@ app.post("/send-mail", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(port, console.log(`initialized on http://localhost:${port}`));
